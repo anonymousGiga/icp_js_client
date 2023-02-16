0b552676695d0fe760d94713b4bc8ff177f0233d
anonymousGiga <cryptonymGong@gmail.com>
2023-02-16 17:24:19 +0800

    add

--- src/node/index.js
+++ src/node/index.js
@@ -3,23 +3,18 @@ import { HttpAgent } from "@dfinity/agent";
 import { canisterId, createActor } from "../declarations/beefy_light_client_on_icp_backend/index.js";
 import { identity } from "./identity.js";
 import { createRequire } from "node:module";
-// import path from "path";
-// import fs from "fs";
-// import mmm from "mmmagic";
-// import { fileURLToPath } from "url";
-// import sha256File from "sha256-file";
-// import { Blob } from "buffer";
+import {Ed25519KeyIdentity} from "@dfinity/identity";
 
 const require = createRequire(import.meta.url);
 const localCanisterIds = require("../../canister_ids.json");
 
-const encoder = new TextEncoder();
+export const aliceIdentity = Ed25519KeyIdentity.generate();
 
 export const host = "http://127.0.0.1:4943";
-// export const host = "https://ic0.app",    //main net will use this
+// export const host = "https://ic0.app";    //mainnet
 const agent = new HttpAgent({
   host,
-  identity: await identity,
+  identity: aliceIdentity,
   fetch,
 });
 const effectiveCanisterId =
